9c400b0d756fd5d73c0e6af25c3954407d2b3513
John Larson <jl@buildmidwestern.com>
2023-08-15 11:22:26 -0500

    chore: updated copy

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     const features = [
         {
             name: 'Managed IT Services',
-            description: 'Level billing at predictable costs',
+            description: 'Level billing. Predictable costs.',
             icon: WrenchScrewdriverIcon,
         },
         {
@@ -34,7 +34,7 @@ export default function Home() {
         },
         {
             name: 'Security Awareness Training',
-            description: 'Reduced data-loss liability and costly breaches with enhanced user-awareness.',
+            description: 'Reduces data-loss liability and costly breaches with enhanced user-awareness.',
             icon: ShieldCheckIcon,
         },
         {
@@ -59,25 +59,25 @@ export default function Home() {
           <nav className="h-auto w-full relative z-50 flex justify-center items-center">
               <div className="w-full md:w-3/4 flex flex-col sm:flex-row justify-between items-center gap-6 md:gap-10 xl:gap-0 p-8">
                   <a href={'tel:+16019273282'}  className="w-1/4 flex flex-row justify-center sm:justify-start items-center gap-2">
-                      <div className="w-full flex flex-row justify-center sm:justify-start items-center gap-4">
-                          <div>
+                      <div className="w-full flex flex-row justify-center sm:justify-start items-center gap-4 text-white">
+                          <div className="text-white">
                               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                                   <path fillRule="evenodd" d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z" clipRule="evenodd" />
                               </svg>
                           </div>
-                          <span className="text-base hover:underline">601.927.3282</span>
+                          <span className="text-base hover:underline text-white">601.927.3282</span>
                       </div>
                   </a>
                   <a href={'mailto:support@andrewb.it'} className="w-1/4 flex flex-row justify-center sm:justify-end items-center gap-2">
-                      <div className="w-full flex flex-row justify-center sm:justify-end items-center gap-4">
-                          <div>
+                      <div className="w-full flex flex-row justify-center sm:justify-end items-center gap-4 text-white">
+                          <div className="text-white">
                               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                                   <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
                                   <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
                               </svg>
 
                           </div>
-                          <span className="text-base hover:underline">support@andrewb.it</span>
+                          <span className="text-base hover:underline text-white">support@andrewb.it</span>
                       </div>
                   </a>
               </div>
