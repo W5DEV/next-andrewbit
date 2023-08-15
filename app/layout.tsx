03c7eec81a88c152a38a3243d8e572b74fc96d51
John Larson <jl@buildmidwestern.com>
2023-08-14 23:54:29 -0500

    feat: updated icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Head from "next/head";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,7 +9,7 @@ export const metadata: Metadata = {
   title: 'Honest IT. Simplified. | ANDREWB.IT',
   description: 'Managed IT Services',
   icons:{
-    icon: '/favicon.ico',
+    icon: '/ab.ico',
   },
 }
 
@@ -19,6 +20,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+          <link rel="shortcut icon" href="/ab.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/ab.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/ab.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/ab.png"/>
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
