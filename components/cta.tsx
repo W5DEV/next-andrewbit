f9910b1db799382673c3dcd359c16e385702c604
John Larson <jl@buildmidwestern.com>
2023-08-18 03:48:01 -0500

    chore: updated eslint

--- components/cta.tsx
+++ components/cta.tsx
@@ -1,6 +1,5 @@
 import { CheckCircleIcon } from "@heroicons/react/20/solid";
 import Link from "next/link";
-import Image from "next/image";
 
 const benefits = [
   "24/7 Emergency Support",
@@ -15,8 +14,7 @@ export default function Cta() {
       <div className="relative isolate">
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="mx-auto flex max-w-2xl flex-col gap-16 bg-white/5 px-6 py-16 ring-1 ring-white/10 sm:rounded-3xl sm:p-8 lg:mx-0 lg:max-w-none lg:flex-row lg:items-center lg:py-20 xl:gap-x-20 xl:px-20">
-            <Image
-              fill
+            <img
               className="h-96 w-full flex-none rounded-2xl object-cover shadow-xl lg:aspect-square lg:h-auto lg:max-w-sm"
               src="https://images.unsplash.com/photo-1564457461758-8ff96e439e83?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=80"
               alt=""
